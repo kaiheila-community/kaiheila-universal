7c65a765d1dcd291160415a04ce71eccc3947fbd
Il Harper <afanyiyu@hotmail.com>
2021-02-15 12:01:16 +0800

    feat: Disable webSecurity

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ function createWindow() {
       preload: path.join(__dirname, 'preload.js'),
       // nodeIntegration: true,
       // nodeIntegrationInWorker: true,
-      // webSecurity: false,
+      webSecurity: false,
     },
   })
 
