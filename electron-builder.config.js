8c29df6d546142c60b44e1e89d08104f0075bdb1
Il Harper <afanyiyu@hotmail.com>
2021-02-15 17:07:08 +0800

    feat: Add Updater

---
+++ electron-builder.config.js
@@ -0,0 +1,53 @@
+module.exports = {
+  productName: '开黑啦',
+  appId: 'cn.kaiheila.kaiheila-uni',
+  directories: {
+    output: '.build',
+    buildResources: 'resources',
+  },
+  files: [
+    'dist/**/*',
+    'resources/**/*',
+    'resources/kaiheila*',
+    'package.json',
+    '**/node_modules/**/*',
+  ],
+  dmg: {
+    contents: [
+      {
+        x: 410,
+        y: 150,
+        type: 'link',
+        path: '/Applications',
+      },
+      {
+        x: 130,
+        y: 150,
+        type: 'file',
+      },
+    ],
+  },
+  mac: {
+    category: 'public.app-category.social-networking',
+    target: 'dmg',
+    darkModeSupport: true,
+    icon: 'resources/kaiheila-uni.icns',
+  },
+  win: {
+    target: 'nsis',
+    icon: 'resources/kaiheila-uni.ico',
+  },
+  linux: {
+    category: 'Network',
+    synopsis: '开黑啦',
+    icon: 'resources/kaiheila-uni.png',
+    target: 'AppImage',
+  },
+  publish: [
+    {
+      provider: 'generic',
+      url: 'https://delivr.vbox.moe/kaiheila-uni/releases/',
+      publishAutoUpdate: true,
+    },
+  ],
+}
--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,51 @@
 import { app, BrowserWindow, Menu, Tray } from 'electron'
+import { autoUpdater } from 'electron-updater'
 import * as path from 'path'
+import log = require('electron-log')
 
 let mainWindow: BrowserWindow
 let tray: Tray
+
+// Config Auto Updater
+autoUpdater.logger = log
+log.info('App starting...')
+
+// Utils
+const quitApp = function () {
+  mainWindow.removeListener('close', preventWindowClose)
+  mainWindow.close()
+  app.quit()
+  app.quit()
+}
 
 const preventWindowClose = function (e: Electron.Event) {
   e.preventDefault()
   mainWindow.hide()
 }
 
+// Config App Menu
+const appMenuTemplate: Electron.MenuItemConstructorOptions[] = [
+  {
+    label: '开黑啦',
+    submenu: [
+      {
+        label: '关于开黑啦 Universal',
+        role: 'about',
+      },
+      {
+        label: '退出',
+        accelerator: process.platform === 'darwin' ? 'Command+Q' : undefined,
+        click: quitApp,
+      },
+    ],
+  },
+]
+
 const iconPath = path.join(__dirname, '../resources/kaiheila-uni.ico')
-const trayMenuTemplate = [
+const trayMenuTemplate: Electron.MenuItemConstructorOptions[] = [
   {
     label: '退出',
-    click: function () {
-      mainWindow.removeListener('close', preventWindowClose)
-      mainWindow.close()
-      app.quit()
-      app.quit()
-    },
+    click: quitApp,
   },
 ]
 
@@ -53,15 +80,17 @@ function initializeWindow() {
 function initializeTray() {
   // Initialize Tray
   tray = new Tray(iconPath)
-  const trayMenu = Menu.buildFromTemplate(trayMenuTemplate)
   tray.setToolTip('开黑啦')
-  tray.setContextMenu(trayMenu)
+  tray.setContextMenu(Menu.buildFromTemplate(trayMenuTemplate))
   tray.on('click', function () {
     mainWindow.show()
   })
 }
 
 app.on('ready', () => {
+  // Initialize App Menu
+  Menu.setApplicationMenu(Menu.buildFromTemplate(appMenuTemplate))
+
   // Initialize Tray
   initializeTray()
 
@@ -70,6 +99,9 @@ app.on('ready', () => {
   app.on('activate', function () {
     if (BrowserWindow.getAllWindows().length === 0) initializeWindow()
   })
+
+  // Auto Update
+  autoUpdater.checkForUpdatesAndNotify()
 })
 
 app.on('window-all-closed', () => {
