083cb06b383308460bca3fa04832d66c8c61a88d
Il Harper <afanyiyu@hotmail.com>
2021-02-16 15:12:34 +0800

    fix: UniShell Bugfix

--- src/shell/uniShell.ts
+++ src/shell/uniShell.ts
@@ -1,4 +1,4 @@
-import { app, BrowserWindow, Menu, nativeImage, Tray } from 'electron'
+import { app, BrowserWindow, Menu, NativeImage, Tray } from 'electron'
 import { pathResolve } from '../utils'
 import { IInitializable } from './types'
 
